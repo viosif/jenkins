11f13f855eb4491412e42b128f6d33e444ba8d5e
IosIF <varga_iosif2005@yahoo.com>
2020-08-27 16:17:40 +0300

    change

--- createJobs.groovy
+++ createJobs.groovy
@@ -16,7 +16,6 @@ pipelineJob('bicycle-docker') {
                         url 'https://github.com/viosif/bicycle.git'
                     }
                     branch 'master'
-                    credentialsId '6148930-ff5a-430e-9e08-016d81ed7058'
                     scriptPath('jenkinsfile')
                 }
             }
